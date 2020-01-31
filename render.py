f437a827cbfbbf8c1da6b5a880bda7dad5334ba2
Luke Robot Hospadaruk <luke.hospadaruk@ithaka.org>
2020-01-31 12:03:52 -0500

    mor interesting

--- render.py
+++ render.py
@@ -54,12 +54,14 @@ def render_board(
         text_height = text_scale*cell_size
         for row in range(board_rows):
             for col in range(board_columns):
-                label = elem("text",
+                label = elem(
+                    "text",
                     x=col*cell_size+col*cell_border+.5*cell_size,
                     y=row*cell_size+row*cell_border+.5*cell_size-text_vpad,
                     font_size=text_height,
                     font_family="sans-serif",
                     text_anchor="middle",
+                    fill="red",
                     dominant_baseline="middle"
                 )
                 label.text="{letter}-{number}".format(letter=chr(ord('A')+row), number=col)
@@ -69,15 +71,17 @@ def render_board(
     def board_outline():
         return elem("rect", width=board_width, height=board_height, rx=corner_radius, style=style)
 
+    def root_node():
+        return elem(
+            "svg",
+            height= "{0}cm".format(board_height),
+            width= "{0}cm".format(board_width),
+            viewBox= "0 0 {board_width} {board_height}".format(**locals()),
+            xmlns="http://www.w3.org/2000/svg",
+            version="1.1",
+            baseProfile="full"
+        )
 
-    doc = elem("svg",
-        height= "{0}cm".format(document_height),
-        width= "{0}cm".format(board_width),
-        viewBox= "0 0 {board_width} {document_height}".format(**locals()),
-        xmlns="http://www.w3.org/2000/svg",
-        version="1.1",
-        baseProfile="full"
-    )
 
     lower_board = et.Element("g")
     lower_board.append(board_outline())
@@ -85,9 +89,7 @@ def render_board(
     lower_labels.append(piece_labels())
     lower_board.append(lower_labels)
 
-    doc.append(lower_board)
-
-    upper_board = elem("g", transform="translate({x} {y})".format(x=0, y=board_height+outer_border))
+    upper_board = elem("g")
     upper_board.append(board_outline())
     upper_board_pieces = elem("g", transform="translate({x} {y})".format(x=outer_border, y=outer_border))
     upper_board_pieces.append(piece_outlines())
@@ -96,18 +98,17 @@ def render_board(
     #upper_board_pieces.append(piece_labels())
     upper_board.append(upper_board_pieces)
 
-    pieces = elem("g", transform="translate({x} {y})".format(x=outer_border, y=board_height*2 + outer_border*2))
+    pieces = elem("g")
     pieces.append(piece_outlines())
     pieces.append(piece_labels())
 
-    doc.append(pieces)
 
-
-    doc.append(upper_board)
-
-
-    #print("<!")
-    sys.stdout.buffer.write(et.tostring(doc, encoding='utf-8', pretty_print=True))
+    for node, filename in [(upper_board, 'upper.svg'), (lower_board, 'lower.svg'), (pieces, 'pieces.svg')]:
+        root = root_node()
+        root.append(node)
+        document = et.ElementTree(root)
+        with open(filename, 'wb') as f:
+            document.write(f, encoding='utf-8', xml_declaration=True)
 
 
 if __name__ == '__main__':
